199b91aa0e69c8c9c384ec11e9ca067336502a4d
Sairam Venkata Krishna Maddula <vksmaddula@virtusa.com>
2018-06-05 13:55:36 +0530

    added the chart representation

--- WeatherApplication/js/weathereport.js
+++ WeatherApplication/js/weathereport.js
@@ -2,6 +2,8 @@
 $("#btn").click(function ()
     {
         var city = $("#cityName").val();
+        //alert(city);
+        $(".table").removeClass("content-hide");
     $.ajax({
 
         type:"GET",
@@ -28,23 +30,73 @@ $("#btn").click(function ()
     });
 })
 
-// Forecasting weather in form of charts
-
-$("#fbtn").click( function () {
+$("#fbtn").click(function (){
 
     var city = $("#cityName").val();
     $.ajax({
 
         type:"GET",
         url: 'http://api.openweathermap.org/data/2.5/forecast?q=' + city + '&APPID=ef0107f6f8d7d8932a3c0d617ef9b1ec',
-        success: function (output){
+        success : function(output){
             
-
+            console.log(output);
+            Dates = output.list.map(function (ele) { return new Date(ele.dt * 1000) });
+            console.log(Dates);
+            Temperatures = output.list.map(function (ele) { return Math.round(ele.main.temp - 273) });
+            console.log(Temperatures);
+            plotChart(Temperatures, Dates);
         },
-        failure:function (output){
-            console.log("in failure scenario");
+        failure : function (output){
             console.log(output);
         }
+
     });
+
+    function plotChart(tempArr, datesArr) {
+        //$('#chart-container').removeClass('results-hide');
+        Highcharts.chart('chart-container', {
+            chart: {
+                type: 'spline'
+            },
+            title: {
+                text: 'Monthly Average Temperature'
+            },
+            xAxis: {
+                categories: datesArr
+            },
+            yAxis: {
+                title: {
+                    text: 'Temperature'
+                },
+                labels: {
+                    formatter: function () {
+                        return this.value + '°';
+                    }
+                }
+            },
+            tooltip: {
+                crosshairs: true,
+                shared: true
+            },
+            plotOptions: {
+                spline: {
+                    marker: {
+                        radius: 4,
+                        lineColor: '#666666',
+                        lineWidth: 1
+                    }
+                }
+            },
+            series: [{
+                name: cityName,
+                marker: {
+                    symbol: 'square'
+                },
+                data: tempArr
+
+            }]
+        });
+    }
+
 
 })
